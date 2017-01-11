9ea7f620b1fd563d0019507afec1130a3f3fcfdd
root <root@test-22.at.office>
2017-01-11 16:32:27 +0800

    注释

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -1,4 +1,8 @@
 #encoding=utf-8
+'''
+    https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    Given a string, find the length of the longest substring without repeating characters.
+'''
 
 def lengthOfLongestSubstring(s):
     """
